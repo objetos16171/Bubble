851363a12961c55b0ba5e85b8b124d1bfe135cb3
AyrtonEs <ay.rton11@hotmail.com>
2016-11-03 19:13:02 -0600

    Conel gira
    Conel gira y ve en la direccion que va

--- ProyectoBubble/Conel.java
+++ ProyectoBubble/Conel.java
@@ -15,19 +15,26 @@ public class Conel extends Actor
      public final int GRAVEDAD=5;
      public final int MOV=2;
      public final int SALTO=5;
+     
     public void act() 
     {
         // Add your action code here.
-        if(Greenfoot.isKeyDown("up")){
+        if(Greenfoot.isKeyDown("up"))
+        {
             setLocation(getX(),getY()-SALTO);  
         }
         
-        if(Greenfoot.isKeyDown("left")){
+        if(Greenfoot.isKeyDown("left"))
+        {
+            setImage("conel_opt2.png");
             setLocation(getX()-MOV,getY());
         }
-        if(Greenfoot.isKeyDown("right")){
+        
+        if(Greenfoot.isKeyDown("right"))
+        {
+            setImage("conel_opt.png");
             setLocation(getX()+MOV,getY());
-        }    
+        }
         
         if(getY()<=380 && !Greenfoot.isKeyDown("up"))
         {
